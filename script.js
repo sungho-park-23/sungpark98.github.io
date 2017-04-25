32b53612c4129c5df1ba80d66c681c8929f35269
Sung Ho Park <sunghopark@Sungs-MacBook-Pro.local>
2017-04-25 03:00:58 -0700

    finished html css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	$("#project1").click(function() {
 		$(".project-description").css("visibility", "visible");
+		$(".project-description").css("z-index", "900");
+		$("#project1description").css("z-index", "980");
 	});
 	$("#project2").click(function() {
 		$(".project-description").css("visibility", "visible");
