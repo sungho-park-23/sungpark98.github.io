b58c2546c320471a44ef4f647027e9e09e23ee3b
Sung Ho Park <sunghopark@Sungs-MacBook-Pro.local>
2017-04-25 02:50:58 -0700

    finished html css

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$("#project1").click(function() {
+		$(".project-description").css("visibility", "visible");
+	});
+	$("#project2").click(function() {
+		$(".project-description").css("visibility", "visible");
+	});
+	$("#project3").click(function() {
+		$(".project-description").css("visibility", "visible");
+	}
+		
+	});
+	$("#project4").click(function() {
+		$(".project-description").css("visibility", "visible");
+	});
+	$("#project5").click(function() {
+		$(".project-description").css("visibility", "visible");
+	});
+});
